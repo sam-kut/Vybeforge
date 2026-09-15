71455b4af51cf85126fe5b20a4974bd60efdf796
sam-kut <kuteyifaith64@gmail.com>
2026-09-15 11:49:03 +0100

    Update __init__.py

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,1 +1,6 @@
+from app.models.user import User
+from app.models.project import Project, ProjectStatus, ProjectType
+from app.models.track import Track, TrackType
+from app.models.stem import Stem
+from app.models.version import ProjectVersion
 
